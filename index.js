6434aab92d131b049b2013433b8c5bd515fad1a0
Peter deHaan <peter@deseloper.com>
2016-09-01 22:37:23 -0700

    Remove wrapper Promise
    The `fetch()` API already returns a Promise, so you can just recycle that. Plus, let any potential errors fall through to the `.catch()` in the calling code.

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ const fetch = require('isomorphic-fetch');
 const Package = require('nice-package');
 
 module.exports = moduleName => {
-  return new Promise((resolve, reject) => {
-    return fetch(`${registryUrl}${moduleName}`)
+  return fetch(`${registryUrl}${moduleName}`)
     .then(resp => resp.json())
-    .then(pkgInfo => resolve(new Package(pkgInfo)))
-    .catch(err => reject(err))
-  })
+    .then(pkgInfo => new Package(pkgInfo));
 }
